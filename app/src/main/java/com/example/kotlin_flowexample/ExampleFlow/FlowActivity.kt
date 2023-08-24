06b3830bd881f671520bbef3202d03670f775654
George Dashkin <siodo@mail.ru>
2023-08-24 15:54:59 +0300

    new commit 24_08_2023_2

---
+++ app/src/main/java/com/example/kotlin_flowexample/ExampleFlow/FlowActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kotlin_flowexample.ExampleFlow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kotlin_flowexample.R
+
+class FlowActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flow)
+    }
+}
